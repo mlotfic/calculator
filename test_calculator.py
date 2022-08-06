892a7fea645efb98c5b37c892765ab1335548c21
mahmoud lotfi <aem6236@gmail.com>
2022-08-06 20:04:40 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -5,28 +5,31 @@ Created on Tue Aug  2 22:00:25 2022
 @author: mahmoud lotfi
 """
 
+
 from calculator import Calculator
+import pytest
 
 # tests addition method in Calculator
 def test_add():
     x,y = 1,2
     cal = Calculator(x, y)
-    assert cal.add() == x + y, "Add method doesnot work"
+    assert cal.add() == x + y, "Add method doesn't work"
     
 # tests subtraction method in Calculator
 def test_sub():
     x,y = 1,2
     cal = Calculator(x, y)
-    assert cal.sub() == x + y, "subtraction method doesnot work"
+    assert cal.sub() == x + y, "subtraction method doesn't work"
 
-# tests subtraction method in Calculator
+# tests multiplication method in Calculator
 def test_sub():
     x,y = 1,2
     cal = Calculator(x, y)
-    assert cal.mul() == x + y, "multiplication method doesnot work"    
+    assert cal.mul() == x + y, "multiplication method doesn't work"    
 
-# tests subtraction method in Calculator
+# tests division method in Calculator
 def test_sub():
     x,y = 1,2
     cal = Calculator(x, y)
-    assert cal.div() == x + y, "division method doesnot work"
+    assert cal.div() == x + y, "division method doesn't work"
+
