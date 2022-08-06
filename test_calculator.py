679ed9e63f1d04ade02af39b07f11d0f17f8f4bd
mahmoud lotfi <aem6236@gmail.com>
2022-08-06 20:03:33 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,20 +2,31 @@
 """
 Created on Tue Aug  2 22:00:25 2022
 
-@author: m
+@author: mahmoud lotfi
 """
 
-from calculator_class import Calculator
+from calculator import Calculator
 
 # tests addition method in Calculator
 def test_add():
     x,y = 1,2
-    instance = Calculator(x, y)
-    assert instance.add() == x + y, "Add method doesnot work"
+    cal = Calculator(x, y)
+    assert cal.add() == x + y, "Add method doesnot work"
     
 # tests subtraction method in Calculator
 def test_sub():
     x,y = 1,2
-    instance = Calculator(x, y)
-    assert instance.sub() == x + y, "subtraction method doesnot work"
-        
+    cal = Calculator(x, y)
+    assert cal.sub() == x + y, "subtraction method doesnot work"
+
+# tests subtraction method in Calculator
+def test_sub():
+    x,y = 1,2
+    cal = Calculator(x, y)
+    assert cal.mul() == x + y, "multiplication method doesnot work"    
+
+# tests subtraction method in Calculator
+def test_sub():
+    x,y = 1,2
+    cal = Calculator(x, y)
+    assert cal.div() == x + y, "division method doesnot work"
