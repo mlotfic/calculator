da3b71aff18e3e5345805cd6cf7f89be6c773eb8
mahmoud lotfi <aem6236@gmail.com>
2022-08-08 22:07:33 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -30,40 +30,37 @@ class Calculator():
     def div(self):
         return self.x / self.y
     
-# print the calculator interface
-print("Select operation::")
-print("0. Exit")
-print("1. Add (+) ")
-print("2. Subtract (-)")
-print("3. Multiply (*)")
-print("4. Divide (/)")
-
-while True:    
-    # take input from the user
-    choice = input("Enter choice(0/1/2/3/4): ")
-    
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        x = (input("Enter first number: "))
-        y = (input("Enter second number: "))
-        
-        # creating object from class
-        cal = Calculator(x, y)
-    
-    if choice == '1':
-        print("Result: ",cal.add())
-    elif choice == '2':
-        print("Result: ",cal.sub())
-    elif choice == '3':
-        print("Result: ",cal.mul())
-    elif choice == '4':
-        print("Result: ",round(cal.div(),2))
-    elif choice == '0':
-        print("Exiting!!")
-        break
-    else:
-        print("Invalid Input, please try again")
-
 if __name__ == '__main__':
-    cal_obj = Calculator(1, 2)
-    print(cal_obj.add())
+    # print the calculator interface
+    print("Select operation::")
+    print("0. Exit")
+    print("1. Add (+) ")
+    print("2. Subtract (-)")
+    print("3. Multiply (*)")
+    print("4. Divide (/)")
+
+    while True:    
+        # take input from the user
+        choice = input("Enter choice(0/1/2/3/4): ")
+        
+        # check if choice is one of the four options
+        if choice in ('1', '2', '3', '4'):
+            x = (input("Enter first number: "))
+            y = (input("Enter second number: "))
+            
+            # creating object from class
+            cal = Calculator(x, y)
+        
+        if choice == '1':
+            print("Result: ",cal.add())
+        elif choice == '2':
+            print("Result: ",cal.sub())
+        elif choice == '3':
+            print("Result: ",cal.mul())
+        elif choice == '4':
+            print("Result: ",round(cal.div(),2))
+        elif choice == '0':
+            print("Exiting!!")
+            break
+        else:
+            print("Invalid Input, please try again")
