2c5e10c41147b1b440793fb1d1c65eabf79766b1
mahmoud lotfi <aem6236@gmail.com>
2022-08-09 14:42:30 +0200

    Create test_calculator.py

---
+++ tests/test_calculator.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 22:00:25 2022
+
+@author: mahmoud lotfi
+"""
+from calculator import Calculator
+
+# tests addition method in Calculator
+def test_add():
+    x,y = 1,2
+    cal = Calculator(x, y)
+    assert cal.add() == x + y, "Add method doesn't work"
+    
+# tests subtraction method in Calculator
+def test_sub():
+    x,y = 1,2
+    cal = Calculator(x, y)
+    assert cal.sub() == x - y, "subtraction method doesn't work"
+
+# tests multiplication method in Calculator
+def test_mul():
+    x,y = 1,2
+    cal = Calculator(x, y)
+    assert cal.mul() == x * y, "multiplication method doesn't work"    
+
+# tests division method in Calculator
+def test_div():
+    x,y = 1,2
+    cal = Calculator(x, y)
+    assert cal.div() == x / y, "division method doesn't work"
+
